36d6f41c44d02488885b40f9b4b37bb71ec12431
Abhirath Mahipal <abhiabhiabhi.27@gmail.com>
2016-01-17 15:08:31 +0530

    Solved one problem

---
+++ 1800 - 1899/1873.c
@@ -0,0 +1,44 @@
+// You have to manually count the number of tournaments 
+// each member took part in. You can use arrays, if statements 
+// or even switch cases to output the required solution.
+
+#include <stdio.h>
+
+int main (void)
+{
+
+	int input;
+	scanf("%d", &input);
+
+	switch(input)
+	{
+		case 0:	printf("5\n");
+				break;
+		case 1:	printf("20\n");
+				break;
+		case 2:	printf("12\n");
+				break;
+		case 3:	printf("2\n");
+				break;
+		case 4:	printf("1\n");
+				break;
+		case 5:	printf("4\n");
+				break;
+		case 6:	printf("6\n");
+				break;
+		case 7:	printf("1\n");
+				break;
+		case 8:	printf("4\n");
+				break;
+		case 9:	printf("4\n");
+				break;
+		case 10: printf("1\n");
+				break;
+		case 11: printf("0\n");
+				break;
+
+
+
+	}
+	return 0;
+}
